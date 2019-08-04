4dd35e4fcaa19bd25fae7046b63495d4156dc639
Kevin Hennessy <khenness@tcd.ie>
2019-08-04 18:42:50 +0100

    add unity project

---
+++ Wendle_Pendlewick/Assets/Script/LevelSwitch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitch : MonoBehaviour {
+
+    //public string levelToLoad;
+    public int levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+        
+        {
+
+        
+
+        if(collision.tag == "Player") 
+        {
+            Debug.Log("hit");
+            SceneManager.LoadSceneAsync(levelToLoad);        }
+    }
+
+    public void LoadLevel (int level) {
+        SceneManager.LoadSceneAsync(level);
+    }
+}
